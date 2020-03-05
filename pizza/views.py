53bf198ddcf7bd3ba02770aeab3818b325e7ec96
ndittren <36773036+ndittren@users.noreply.github.com>
2020-03-05 15:32:36 -0500

    feat: beginnings of formsets

--- pizza/views.py
+++ pizza/views.py
@@ -1,16 +1,35 @@
 from django.shortcuts import render
-from .forms import PizzaForm
+from .forms import PizzaForm, MultiplePizzaForm
+from django.forms import formset_factory
 
 def home(request):
     return render(request, 'pizza/home.html')
 
 def order(request):
+    multiple_form = MultiplePizzaForm()
     if request.method == 'POST':
         filledform = PizzaForm(request.POST)
         if filledform.is_valid():
             note = 'Thanks for ordering! Your %s %s and %s pizza is on its way!' %(filledform.cleaned_data['size'], filledform.cleaned_data['topping1'], filledform.cleaned_data['topping2'], )
             new_form = PizzaForm()
-            return render(request, 'pizza/order.html', {'pizzaform': new_form, 'note': note})
+            return render(request, 'pizza/order.html', {'pizzaform': new_form, 'note': note, 'multiple_form': multiple_form})
     else:
         form = PizzaForm()
-        return render(request, 'pizza/order.html', {'pizzaform': form})
+        return render(request, 'pizza/order.html', {'pizzaform': form, 'multiple_form': multiple_form})
+
+def pizzas(self):
+    number_of_pizzas = 2
+    filled_multiple_pizza_form = MultiplePizzaForm(request.GET)
+    if filled_multiple_pizza_form.is_valid():
+        number_of_pizzas = filled_multiple_pizza_form.cleaned_data['number']
+    PizzaFormSet = formset_factory(PizzaForm, extra=number_of_pizzas)
+    formset = PizzaFormSet()
+    if request.method == 'POST':
+        filled_formset = PizzaFormSet(request.POST)
+        if filled_formset.is_valid():
+            for form in filled_formset:
+                print(form.cleaned_data['topping1'])
+            note = 'Pizza have been ordered!'
+        else:
+            note= 'Order was not created, please try again'
+        return render(request, 'pizza/pizzas.html', {'note': note, 'formset': formset})
